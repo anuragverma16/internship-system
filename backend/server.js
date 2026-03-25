1b0970f4adbb1e4dee2c4d9bddd2935cd6104c2a
anuragverma808575 <anuragdhakhaiya@gmail.com>
2026-03-25 20:46:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -106,8 +106,7 @@ const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
   app.listen(PORT, () => {
     // console.log(`🚀 Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
-     console.log(`Server running on port ${PORT}`);
-    console.log(`📚 API Documentation: http://localhost:${PORT}/api/health`);
+    console.log(`https://internship-system-server.onrender.com`);
   });
 });
 
